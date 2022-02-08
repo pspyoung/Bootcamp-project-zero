f868864a71d0130548c6615d15d844ae99a09bd1
pspyoung <pspyoung2@gmail.com>
2022-02-08 15:08:14 -0800

    Added html skeleton, buttons, startGame button responsiveness

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+console.log("%c [project-zero app.js] loaded 🧸", "color:white; background-color:brown; padding:5px;");
+
+let eat = 10; // button class eat
+let sleep = 8; // button class sleep
+let hug = 6; // buton class hug
+
+// === Names of Things === //
+// Start Button = $("#startGame"); // 
+// Eat Button = $(".eat");          // $("#eatTimer");
+// Sleep Button = $(".sleep");      // $("#sleepTimer");
+// Hug Button = $(".hug");          // $("#hugTimer");
+
+
+// === Buttons, Timers === //
+
+$('#startGame').on('click', () => {
+console.log ("It started!");
+});
+
+const eatTimer = () => {
+    time--; 
+    $("#eatTimer").text("timer: " + time);
+};
+
+const sleepTimerGo = () => {
+    time--; 
+    $("#sleepTimer").text("timer: " + time);
+};
+
+const hugTimerGo = () => {
+    time--; 
+    $("#hugTimer").text("timer: " + time);
+};
 
