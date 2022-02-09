c63b84a8aa5d53506c8ae4529b96cb0736cc9436
pspyoung <pspyoung2@gmail.com>
2022-02-08 22:00:19 -0800

    Adds functioning age timer. All three metrics now have working buttons, timers, and game ending condition.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 console.log("%c [project-zero app.js] loaded 🧸", "color:white; background-color:brown; padding:5px;");
 
-let eat = 10; // button class eat
+let eat = 0; // button class eat
 let sleep = 0; // button class sleep
 let hug = 0; // buton class hug
+let time = 0; 
+let ageInterval;
 let eatInterval;
+let sleepInterval;
+let hugInterval;
 let species; 
 
 
@@ -18,56 +22,91 @@ let species;
 
 $('#startGame').on('click', () => {
     // console.log ("It started!");
-    eat = 500; 
-    eatInterval = setInterval(eatTimer, 1000);
-    ageTimer();
-    setInterval(ageTimer,)
-    console.log("I'm getting older"!);
+    time = 1; 
+    ageInterval = setInterval(ageTimer, 1000);
+    // eat = 10; 
+    // eatInterval = setInterval(eatTimer, 1000);
+    // sleep = 10;
+    // sleepInterval = setInterval(sleepTimer, 1000);
+    hug = 10;
+    hugInterval = setInterval(hugTimer, 1000);
 });
 
 // === Age Timer
 
 function ageTimer () {
-    time = 0;
     time ++; 
-    // if (time <5) {
-    //     $("#species").text("Baby Bear");
-    // } else if (time >=6 && time < 10) {
-    //     $("#species").text("Kid Bear");
-    // } else {
-    //     $("#species").text("Growed Up Bear");
-    // }
+    $("#ageTimer").text(`${time}`);
+    if (time <5) {
+        $("#species").text("Baby Bear");
+    } else if (time < 10) {
+        $("#species").text("Kid Bear");
+    } else {
+        $("#species").text("Growed Up Bear");
+    }
 }; 
+    
+function stopAgeInterval() {
+    clearInterval(ageInterval);
+};
 
-// === Eat Timer
+    // === Eat Timer
 function eatTimer () {
     eat--; 
     $("#eatTimer").text(`${eat}`);
     if (eat===0) {
-        stopEatInterval();
-        return alert("You have died of hunger");
-    }
-};
+        stopEatInterval(); 
+        stopAgeInterval();
+        alert("You have died of hunger"); 
+    };
+}
 
 $(".eat").on("click", () => {
     eat++;
 });
+
+
 
 function stopEatInterval() {
     clearInterval(eatInterval); 
 }
 
 // === Sleep Timer
-const sleepTimerGo = () => {
-    time = 30;
-    time--; 
-    $("#sleepTimer").text("Wokeness level: " + sleep);
+function sleepTimer () {
+    sleep--; 
+    $("#sleepTimer").text(`${sleep}`);
+    if (sleep===0) {
+        stopSleepInterval();
+        stopAgeInterval(); 
+        alert("You went into deep hybearnation! Better luck next time!");
+    }
 };
+
+function stopSleepInterval() {
+    clearInterval(sleepInterval);
+}
+
+$(".sleep").on("click", () => {
+    sleep++;
+});
 
 // === Hug Timer
 
-const hugTimerGo = () => {
-    time--; 
-    $("#hugTimer").text("Loneliness level: " + time);
+function hugTimer () {
+    hug--; 
+    $("#hugTimer").text(`${hug}`);    
+    if (hug ===0) {
+        stopHugInterval();
+        stopAgeInterval();
+        alert("You need moar bearhugs. Try again :(")
+    }
 };
+
+function stopHugInterval() {
+    clearInterval(hugInterval);
+}
+
+$(".hug").on("click", () => {
+    hug++;
+});
 
