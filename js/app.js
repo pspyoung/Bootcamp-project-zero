bb78f5ac4ceec5a1e0da0884b049f4664b6bfa76
pspyoung <pspyoung2@gmail.com>
2022-02-08 19:44:22 -0800

    Added button CSS. Starting aging functionality

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let eat = 10; // button class eat
 let sleep = 0; // button class sleep
 let hug = 0; // buton class hug
 let eatInterval;
+let species; 
 
 
 // === Names of Things === //
@@ -17,9 +18,26 @@ let eatInterval;
 
 $('#startGame').on('click', () => {
     // console.log ("It started!");
-    eat = 3; 
+    eat = 500; 
     eatInterval = setInterval(eatTimer, 1000);
+    ageTimer();
+    setInterval(ageTimer,)
+    console.log("I'm getting older"!);
 });
+
+// === Age Timer
+
+function ageTimer () {
+    time = 0;
+    time ++; 
+    // if (time <5) {
+    //     $("#species").text("Baby Bear");
+    // } else if (time >=6 && time < 10) {
+    //     $("#species").text("Kid Bear");
+    // } else {
+    //     $("#species").text("Growed Up Bear");
+    // }
+}; 
 
 // === Eat Timer
 function eatTimer () {
@@ -30,6 +48,10 @@ function eatTimer () {
         return alert("You have died of hunger");
     }
 };
+
+$(".eat").on("click", () => {
+    eat++;
+});
 
 function stopEatInterval() {
     clearInterval(eatInterval); 
