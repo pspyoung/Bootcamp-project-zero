62a97afb3ea2e78a15b01e068b50eabe34cb2e72
pspyoung <pspyoung2@gmail.com>
2022-02-08 16:58:43 -0800

    Timer stops when it reaches 0.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 console.log("%c [project-zero app.js] loaded 🧸", "color:white; background-color:brown; padding:5px;");
 
 let eat = 10; // button class eat
-let sleep = 8; // button class sleep
-let hug = 6; // buton class hug
+let sleep = 0; // button class sleep
+let hug = 0; // buton class hug
+let eatInterval;
+
 
 // === Names of Things === //
 // Start Button = $("#startGame"); // 
@@ -14,21 +16,36 @@ let hug = 6; // buton class hug
 // === Buttons, Timers === //
 
 $('#startGame').on('click', () => {
-console.log ("It started!");
+    // console.log ("It started!");
+    eat = 3; 
+    eatInterval = setInterval(eatTimer, 1000);
 });
 
-const eatTimer = () => {
-    time--; 
-    $("#eatTimer").text("timer: " + time);
+// === Eat Timer
+function eatTimer () {
+    eat--; 
+    $("#eatTimer").text(`${eat}`);
+    if (eat===0) {
+        stopEatInterval();
+        return alert("You have died of hunger");
+    }
 };
 
+function stopEatInterval() {
+    clearInterval(eatInterval); 
+}
+
+// === Sleep Timer
 const sleepTimerGo = () => {
+    time = 30;
     time--; 
-    $("#sleepTimer").text("timer: " + time);
+    $("#sleepTimer").text("Wokeness level: " + sleep);
 };
+
+// === Hug Timer
 
 const hugTimerGo = () => {
     time--; 
-    $("#hugTimer").text("timer: " + time);
+    $("#hugTimer").text("Loneliness level: " + time);
 };
 
